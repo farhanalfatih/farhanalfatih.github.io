bc09fee018785a8f666a1df08137b420c219688e
farhanalfatih <163872469+farhanalfatih@users.noreply.github.com>
2024-11-09 15:06:14 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+ // JavaScript to toggle the burger menu
+ function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show'); // Toggle 'show' class
+  }
+
+function tampilkanAlert() {
+    alert('belum ada cv!');
+  }
