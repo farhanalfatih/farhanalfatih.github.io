abb44df06ef1d17b817c9e40e006162583e0df72
farhanalfatih <163872469+farhanalfatih@users.noreply.github.com>
2025-01-12 12:49:11 +0700

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
- // JavaScript to toggle the burger menu
- function toggleMenu() {
-    const navLinks = document.getElementById('nav-links');
-    navLinks.classList.toggle('show'); // Toggle 'show' class
-  }
-
-function tampilkanAlert() {
-    alert('belum ada cv!');
-  }
